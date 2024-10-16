b643dd5ddc904e16c61b555b4b79a7934c1d5c34
Ali-abm19 <hugeali643@gmail.com>
2024-10-16 14:39:20 +0300

    level 1 and level 2

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
 
 const btn = document.getElementById("btn");
 const itemsList = document.getElementById("currentItems");
+const counterComplete = document.getElementById("completed");
+const counterAll = document.getElementById("AllItems");
 
-// btn.onclick(() => {
-//     let item = document.getElementById("itemToAdd");
-//     let itemWrapper = document.createElement("div");
-
-//     let i = `<p>${item}</p>`
-//     itemWrapper.innerHTML.appendChild(i);
-//     console.log(itemWrapper);
-//     itemsList.innerHTML.appendChild(itemWrapper);
-// });
+// localStorage(itemsList);
 
 btn.addEventListener("click", () => {
     const item = document.getElementById("itemToAdd");
     const itemWrapper = document.createElement("div");
     const toDo = `
-    <span class="textItem">${item.value}</span> <input class="box" type="checkbox"> <i class="material-icons">delete</i>`;
+    <span class="textItem">${item.value}</span>
+    <input class="box" type="checkbox">
+    <i class="material-icons deleteIcon">delete</i>
+    <i class="material-icons editIcon">edit</i>`;
     itemWrapper.innerHTML = toDo;
     itemsList.appendChild(itemWrapper);
+    counterAll.innerHTML++;
     item.value = "";
 
 
     const checkbox = itemWrapper.querySelector(".box");
     const textToStrike = itemWrapper.querySelector(".textItem");
     checkbox.addEventListener("change", () => {
-        if (checkbox.checked == true)
+        if (checkbox.checked == true) {
+            counterComplete.innerHTML++;
+            console.log(counterComplete);
             textToStrike.style.textDecoration = "line-through";
-        else
+        }
+        else {
+            counterComplete.innerHTML--;
             textToStrike.style.textDecoration = "none";
+        }
     });
 
-    const icon = itemWrapper.querySelector(".material-icons");
-    icon.addEventListener("click", ()=>{
+    const deleteIcon = itemWrapper.querySelector(".deleteIcon");
+    deleteIcon.addEventListener("click", () => {
         itemWrapper.innerHTML = null;
+        counterAll.innerHTML--;
+        if (checkbox.checked == true) {
+            counterComplete.innerHTML--;
+        }
+    });
+
+    const edit = itemWrapper.querySelector(".editIcon");
+    edit.addEventListener("click", () => {
+        let newText = prompt("provide the new name: ");
+        textToStrike.innerHTML = newText;
     });
 });
