1f7f774bc7676bb1f0a891cd616fdb5e24e70414
Ali-abm19 <hugeali643@gmail.com>
2024-10-17 14:14:34 +0300

    Local Storage

--- script.js
+++ script.js
@@ -4,7 +4,58 @@ const itemsList = document.getElementById("currentItems");
 const counterComplete = document.getElementById("completed");
 const counterAll = document.getElementById("AllItems");
 
-// localStorage(itemsList);
+let localStorage_list = [];
+let retrieved_LocalStorage_list = JSON.parse(localStorage.getItem("list"));
+
+retrieved_LocalStorage_list.map(toDoItemFromLS => {
+    const itemWrapper = document.createElement("div");
+    const toDo = `
+    <span class="textItem">${toDoItemFromLS}</span>
+    <input class="box" type="checkbox">
+    <i class="material-icons deleteIcon">delete</i>
+    <i class="material-icons editIcon">edit</i>`;
+    if (toDoItemFromLS != "") {
+        itemWrapper.innerHTML = toDo;
+        itemsList.appendChild(itemWrapper); localStorage_list.push(toDoItemFromLS);
+        counterAll.innerHTML++;
+    }
+    const checkbox = itemWrapper.querySelector(".box");
+    const textToStrike = itemWrapper.querySelector(".textItem");
+    if (checkbox != null) {
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked == true) {
+                counterComplete.innerHTML++;
+                textToStrike.style.textDecoration = "line-through";
+            }
+            else {
+                counterComplete.innerHTML--;
+                textToStrike.style.textDecoration = "none";
+            }
+        });
+
+        const deleteIcon = itemWrapper.querySelector(".deleteIcon");
+        deleteIcon.addEventListener("click", () => {
+            itemWrapper.innerHTML = null;
+            localStorage_list.splice([localStorage_list.findIndex(x => x == textToStrike.innerHTML.toString())],1);
+            localStorage.setItem("list", JSON.stringify(localStorage_list));
+            counterAll.innerHTML--;
+            if (checkbox.checked == true) {
+                counterComplete.innerHTML--;
+            }
+        });
+
+        const edit = itemWrapper.querySelector(".editIcon");
+        edit.addEventListener("click", () => {
+            let newText = prompt("provide the new name: ");
+            if (newText != null && newText.length > 0) {
+                localStorage_list[localStorage_list.findIndex(x => x == textToStrike.innerHTML.toString())] = newText;
+                textToStrike.innerHTML = newText;
+                localStorage.setItem("list", JSON.stringify(localStorage_list));
+            }
+        });
+    }
+});
+
 
 btn.addEventListener("click", () => {
     const item = document.getElementById("itemToAdd");
@@ -16,36 +67,40 @@ btn.addEventListener("click", () => {
     <i class="material-icons editIcon">edit</i>`;
     if (item.value != "") {
         itemWrapper.innerHTML = toDo;
-        itemsList.appendChild(itemWrapper);
+        itemsList.appendChild(itemWrapper); localStorage_list.push(item.value);
         counterAll.innerHTML++;
         item.value = "";
     }
 
     const checkbox = itemWrapper.querySelector(".box");
     const textToStrike = itemWrapper.querySelector(".textItem");
-    checkbox.addEventListener("change", () => {
-        if (checkbox.checked == true) {
-            counterComplete.innerHTML++;
-            textToStrike.style.textDecoration = "line-through";
-        }
-        else {
-            counterComplete.innerHTML--;
-            textToStrike.style.textDecoration = "none";
-        }
-    });
+    if (checkbox != null) {
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked == true) {
+                counterComplete.innerHTML++;
+                textToStrike.style.textDecoration = "line-through";
+            }
+            else {
+                counterComplete.innerHTML--;
+                textToStrike.style.textDecoration = "none";
+            }
+        });
 
-    const deleteIcon = itemWrapper.querySelector(".deleteIcon");
-    deleteIcon.addEventListener("click", () => {
-        itemWrapper.innerHTML = null;
-        counterAll.innerHTML--;
-        if (checkbox.checked == true) {
-            counterComplete.innerHTML--;
-        }
-    });
+        const deleteIcon = itemWrapper.querySelector(".deleteIcon");
+        deleteIcon.addEventListener("click", () => {
+            itemWrapper.innerHTML = null;
+            counterAll.innerHTML--;
+            if (checkbox.checked == true) {
+                counterComplete.innerHTML--;
+            }
+        });
 
-    const edit = itemWrapper.querySelector(".editIcon");
-    edit.addEventListener("click", () => {
-        let newText = prompt("provide the new name: ");
-        textToStrike.innerHTML = newText;
-    });
+        const edit = itemWrapper.querySelector(".editIcon");
+        edit.addEventListener("click", () => {
+            let newText = prompt("provide the new name: ");
+            if (newText.length > 0)
+                textToStrike.innerHTML = newText;
+        });
+    }
+    localStorage.setItem("list", JSON.stringify(localStorage_list));
 });
